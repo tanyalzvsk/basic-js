00b10bb34ac2a0c98b683a68187a8000d5655bba
tanya <ttnlzvsk@mail.ru>
2023-04-16 23:21:06 +0300

    feat: add some solutions

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSumOfDigits(n) {
+  let counter = 0;
+  let numArr = n.toString(10).split('').map(int => parseInt(int, 10));
+  while (numArr.length > 0) {
+  for (let i = 0; i < numArr.length; i++) {
+      if (numArr[i] == 9){
+          return numArr[numArr.length - 1];
+      }
+     counter += numArr[numArr.length - 1];
+     numArr.splice(numArr.length - 1);
+     
+        }
+        
+    }
+    return counter;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const transformed = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if ( arr[i-1] !== '--discard-next') {
+        transformed.pop();
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (arr[i-1] !== '--discard-next') {
+        transformed.push(arr[i-1]);
+         } 
+      }
+      else if (arr[i] === '--double-next') {
+      if (arr[i+1] !== null) {
+        transformed.push(arr[i+1]);
+      }
+    } else {
+      transformed.push(arr[i]);
+    }
+  }
+
+  return transformed;
 }
+
 
 module.exports = {
   transform
